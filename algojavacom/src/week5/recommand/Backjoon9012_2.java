c64c60c30242dd1cd6a1a8d8530c97f1855b0b0a
codesche <codesche@gmail.com>
2024-07-30 19:04:33 +0900

    solve: [BOJ] 괄호_240730

--- algojavacom/src/week5/recommand/Backjoon9012_2.java
+++ algojavacom/src/week5/recommand/Backjoon9012_2.java
@@ -9,7 +9,6 @@ public class Backjoon9012_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        Stack stack = new Stack<>();
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
